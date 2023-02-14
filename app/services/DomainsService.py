0179683e64e5302d3678ceb1e50d6811192cb489
Nayzow <thibo.tanguy@gmail.com>
2023-02-15 00:18:19 +0100

    handle errors

--- app/services/DomainsService.py
+++ app/services/DomainsService.py
@@ -9,72 +9,64 @@ class DomainsService:
     URL_MX_API = "https://dnstwister.report/api/mx/"
 
     @staticmethod
+    def handle_request_errors(response):
+        try:
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as errh:
+            return {"error": f"HTTP error: {errh}"}
+        except requests.exceptions.ConnectionError as errc:
+            return {"error": f"Connection error: {errc}"}
+        except requests.exceptions.Timeout as errt:
+            return {"error": f"Timeout error: {errt}"}
+        except requests.exceptions.RequestException as err:
+            return {"error": f"Unknown error: {err}"}
+
+    @staticmethod
     async def convert_to_hexadecimal(domain):
         url = DomainsService.URL_DOMAIN_TO_HEXA_API + domain
-        response = requests.get(url).json()["domain_as_hexadecimal"]
-        if response:
-            return response
-        else:
-            return "No data found"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)["domain_as_hexadecimal"]
 
     @staticmethod
     async def find_data_by_domain(domain):
         url = DomainsService.URL_DOMAIN_TO_HEXA_API + domain
-        response = requests.get(url).json()["domain_as_hexadecimal"]
-        if response:
-            return response
-        else:
-            return "No data found"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)
 
     @staticmethod
     async def find_all_phishing_sites_by_domain(domain):
         hexadecimal = await DomainsService.convert_to_hexadecimal(domain)
         url = DomainsService.URL_PHISHING_API + hexadecimal
-        response = requests.get(url).json()["fuzzy_domains"]
-        if response:
-            return response
-        else:
-            return "No data found"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)
 
     @staticmethod
     async def find_all_phishing_sites_by_domain_hexadecimal(hexadecimal):
         url = DomainsService.URL_PHISHING_API + hexadecimal
-        response = requests.get(url).json()["fuzzy_domains"]
-        if response:
-            return response
-        else:
-            return "No data found"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)
 
     @staticmethod
     async def find_ip_by_domain(domain):
         hexadecimal = await DomainsService.convert_to_hexadecimal(domain)
         url = DomainsService.URL_IP_API + hexadecimal
-        response = requests.get(url).json()["ip"]
-        if response:
-            return response
-        else:
-            return "No data found"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)["ip"]
 
     @staticmethod
     async def find_location_by_ip(ip):
-        if ip:
-            url = DomainsService.URL_LOCATION_API + ip + "?fields=continent,country,regionName,city,zip,lat,lon,timezone,currency,isp,org,reverse,mobile,proxy,hosting,query"
-            response = requests.get(url).json()
-            if response:
-                return response
-            else:
-                return "No data found"
+        url = DomainsService.URL_LOCATION_API + ip + "?fields=continent,country,regionName,city,zip,lat,lon,timezone,currency,isp,org,reverse,mobile,proxy,hosting,query"
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)
 
     @staticmethod
     async def find_location_by_domain(domain):
         ip = await DomainsService.find_ip_by_domain(domain)
-        return await DomainsService.find_location_by_ip(ip)
+        return await DomainsService.find_location_by_ip(ip) if ip else {"error": "No IP found"}
 
     @staticmethod
     async def find_available_domain(domain):
         url = DomainsService.URL_MX_API + await DomainsService.convert_to_hexadecimal(domain)
-        response = requests.get(url).json()
-        if response["mx"]:
-            return False
-        else:
-            return True
+        response = requests.get(url)
+        return DomainsService.handle_request_errors(response)["mx"] == []
